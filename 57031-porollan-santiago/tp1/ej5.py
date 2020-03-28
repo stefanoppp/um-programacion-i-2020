257d481765aef68ee1a139c84942877d8e0017d1
porollansantiago <santiporollan@gmail.com>
2020-03-28 18:07:34 -0300

    arreglado, ahora con input

--- 57031-porollan-santiago/tp1/ej5.py
+++ 57031-porollan-santiago/tp1/ej5.py
@@ -1,15 +1,22 @@
-def p(r,l, c=1):
-    if c <= r:
-        print(l[:c])
-        p(r, l, c+1)
+def p(r,l, c):
+    if c != -1:
+        print(l[c:])
+        p(r, l, c-1)
 
 def get_l(r):
     a = 1
     l = []
     for _ in range(1, r+1):
-        l.append(a)
+        l.insert(0, a)
         a+=2
     return l
 
 if __name__ == "__main__":
-    p(5, get_l(5))
+    while 1:
+        try:
+            r = int(input("rango: "))
+        except:
+            print("ingrese numero")
+        else:
+            break
+    p(r, get_l(r), r-1)
