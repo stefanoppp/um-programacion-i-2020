818a2c4b03d413ac1e2f77053f99e6937112e7a0
PvonK <philippvonkesselstatt@gmail.com>
2020-04-02 21:16:02 -0300

    TP1 Completo orientado a objetos

--- 58164-von-Kesselstatt-Philipp/TP1/Objetos/ejercicio2.py
+++ 58164-von-Kesselstatt-Philipp/TP1/Objetos/ejercicio2.py
@@ -1,20 +1,74 @@
-vegetariano = input("quere una pizza vegetariana? s/n")
-print("eliga un ingrediente: ")
+class Pizza():
+    def __init__(self, ingrediente=None):
+        self.ingrediente = ingrediente
+        self.ingredientesV = ["Pimiento", "Tofu"]
+        self.ingredientesNV = ["Peperoni", "Jamón", "Salmón"]
 
-if vegetariano == "s":
-    print("Pimiento")
-    print("Tofu")
-    pizza = "es vegetariana"
+    def getIngrediente(self):
+        return self.ingrediente
 
-else:
-    print("Peperoni")
-    print("Jamón")
-    print("Salmón")
-    pizza = "no es vegetariana"
+    def setIngrediente(self, ingrediente):
+        self.ingrediente = ingrediente
 
-ingrediente = input("")
+    def esVegetariana(self):
+        if self.ingrediente in self.ingredientesV:
+            return True
+        else:
+            return False
 
-print("\nSu pizza " + pizza + " y lleva los siguentes ingredientes:")
-print("Mozzarella")
-print("Tomate")
-print(ingrediente)
+    def getIngredientesV(self):
+        return self.ingredientesV
+
+    def getIngredientesNV(self):
+        return self.ingredientesNV
+
+
+class Interfaz():
+
+    def __init__(self):
+        self.pizza = Pizza()
+
+    def preguntarV(self):
+        inputV = input("quere una pizza vegetariana? s/n ")
+        while inputV != "s" and inputV != "n":
+            if inputV == "s":
+                return True
+            elif inputV == "n":
+                return False
+            else:
+                input("quere una pizza vegetariana? ingrese s o n ")
+
+    def preguntarIngrediente(self):
+        if self.preguntarV():
+            self.ingredientes = self.pizza.getIngredientesV()
+        else:
+            self.ingredientes = self.pizza.getIngredientesNV()
+
+        print("\nEliga un ingrediente: ")
+        for item in self.ingredientes:
+            print(item, item[0])
+
+        self.choices = [item[0].lower() for item in self.ingredientes]
+        self.choice = ""
+
+        while (self.choice not in self.choices):
+
+            self.choice = input("\nElija la inicial del ingrediente  ").lower()
+
+        self.pizza.setIngrediente(self.ingredientes[self.choices.index(self.choice)])
+
+    def imprimirPizza(self):
+
+        self.vegStatement = " es vegetariana y" if self.pizza.esVegetariana() else ""
+
+        print("\nSu pizza" + self.vegStatement + " lleva los siguentes ingredientes:")
+        print("Muzzarella")
+        print("Tomate")
+        print(self.pizza.getIngrediente())
+
+
+"""
+ejercicio2 = Interfaz()
+ejercicio2.preguntarIngrediente()
+ejercicio2.imprimirPizza()
+"""
