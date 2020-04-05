b0c64056e894342d59084ff9ae0c6ac286aa6f4e
porollansantiago <santiporollan@gmail.com>
2020-04-05 15:59:47 -0300

    mas corto

--- 57031-porollan-santiago/tp1/ej3.py
+++ 57031-porollan-santiago/tp1/ej3.py
@@ -1,17 +1,6 @@
-impares = lambda num: [x for x in range(num+1) if x%2 == 1]
+
 
 if __name__ == "__main__":
-    while 1:
-        try:
-            r = int(input("rango: "))
-        except:
-            print("ingrese numero")
-        else:
-            break
-    print("impares hasta el ", r, ": ")
-    im = impares(r)
-    for num in im[:-1]:
-        print(num, end=", ")
-    print(im[-1])
-        
+    r = int(input("rango: "))
+    print("impares hasta el " + str(r) + ": " + ", ".join([str(x) for x in range(r+1) if x%2 == 1]))
         
