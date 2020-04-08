b2b9650810cf0013e55b079234b327377682dd5d
porollansantiago <santiporollan@gmail.com>
2020-04-08 00:26:02 -0300

    agregado ej13nuevo

--- 57031-porollan-santiago/tp1/copia_nueva/ej13_nuevo.py
+++ 57031-porollan-santiago/tp1/copia_nueva/ej13_nuevo.py
@@ -3,11 +3,12 @@ from random import randint
 
 class Numeros():
     def show(self):
+        suma = 0
         for _ in range(10):
             r = randint(0, 9)
             print(r)
             suma += r
-            print("promedio: ", suma / 10)
+        print("promedio: ", suma / 10)
 
 
 if __name__ == "__main__":
